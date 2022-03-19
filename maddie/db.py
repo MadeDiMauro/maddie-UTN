54d53035246b7a3bfa481b963f51b1264f80bb69
Madeleine Di Mauro <damaris.dmauro@gmail.com>
2022-03-18 23:11:29 -0300

    setup heroku

--- maddie/db.py
+++ maddie/db.py
@@ -1,10 +1,10 @@
 POSTGRESQL = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'maddie',
-        'USER': 'postgres',
-        'PASSWORD':'admin',
-        'HOST' : 'localhost',
+        'NAME': 'd2b489tpv9ooij',
+        'USER': 'smciwepwpwagvi',
+        'PASSWORD':'a5243265746577d8119626dbec42aec4cc73d2da6ebb5a39b12dd069fd3f8e04',
+        'HOST' : 'ec2-3-222-204-187.compute-1.amazonaws.com',
         'PORT':'5432',
     }
 }
